e26d121f5aa0951cdd6356651251600420ba80da
Mochammad Rizky Diprasetya <diprasetya.rizky@protonmail.com>
2023-02-28 09:20:26 +0100

    add setup.py

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="mlpro_mpps",
+    version="1.0.1",
+    description="MLPro-MPPS - A Customizable Multi-Purpose Production System in Python",
+    author="MLPro Team",
+    author_mail="mlpro@listen.fh-swf.de",
+    license="MIT",
+    packages=["mlpro_mpps"],
+    zip_safe=False,
+)
