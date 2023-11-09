fb9dd63b4a40b620ee7a9420445048f70e6e2d1b
steveyuwono <steveyuwono@gmail.com>
2023-11-09 17:19:14 +0100

    prepare template for howto 008 #47

---
+++ src/mlpro_mpps/examples/howto_008_run_GT_on_LS_BGLP_using_MPPS.py
@@ -0,0 +1,23 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro_mpps.examples
+## -- Module  : howto_008_run_GT_on_LS_BGLP_using_MPPS.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-11-09  0.0.0     SY       Creation
+## -- 2023-??-??  1.0.0     SY       Release of first version
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2023-??-??)
+
+This example demonstrates the implementation of the MPPS-based Larger-Scale BGLP as an GT Game Board.
+
+You will learn:
+    
+    1) How to set up a built-in MPPS to a RL environment.
+    
+    2) How to set up GT game and training, including players, random policies, etc.
+    
+"""
