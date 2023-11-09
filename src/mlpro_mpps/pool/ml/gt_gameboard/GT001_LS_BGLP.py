7bc7ec29f3e110fc208782650b56fdf4fc32f134
steveyuwono <steveyuwono@gmail.com>
2023-11-09 17:25:39 +0100

    creation GT001_LS_BGLP.py #47

---
+++ src/mlpro_mpps/pool/ml/gt_gameboard/GT001_LS_BGLP.py
@@ -0,0 +1,21 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
+## -- Package : mlpro_mpps.pool.gt_gameboard
+## -- Module  : GT001_LS_BGLP.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2023-11-09  0.0.0     SY       Creation
+## -- 2023-??-??  1.0.0     SY       Release of first version
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 1.0.0 (2023-??-??)
+
+This module provides a default implementation of the BGLP in MLPro-MPPS as RL Environment.
+"""
+
+
+from mlpro_mpps.pool.mpps.PS003_LS_BGLP import LS_BGLP
+from mlpro.bf.math import *
+from mlpro.gt.models import *
