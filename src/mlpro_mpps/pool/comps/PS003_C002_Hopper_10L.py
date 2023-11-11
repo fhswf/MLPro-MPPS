125036b37799745d564aae39b2603979cf2f011e
steveyuwono <steveyuwono@gmail.com>
2023-11-11 21:19:13 +0100

    PS003_C002_Hopper_10L.py

--- src/mlpro_mpps/pool/comps/PS003_C002_Hopper_10L.py
+++ src/mlpro_mpps/pool/comps/PS003_C002_Hopper_10L.py
@@ -1,7 +1,7 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
 ## -- Package : mlpro_mpps.pool.comps
-## -- Module  : PS003_C002_Hopper_9L.py
+## -- Module  : PS003_C002_Hopper_10L.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
@@ -12,7 +12,7 @@
 """
 Ver. 1.0.0 (2023-11-11)
 
-This module provides a default implementation of a component of the BGLP, which is a 9.1L Mini Hopper.
+This module provides a default implementation of a component of the BGLP, which is a 10L Mini Hopper.
 A hopper is a component to temporary store materials that consists of a sensor.
 """
 
@@ -28,22 +28,22 @@ import sys
             
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Hopper9_Sensor(SimSensor):
+class Hopper10_Sensor(SimSensor):
     """
     This class serves as a sensor in the upper side of the hopper to indicate whether the fill-level
     of the hopper closes to overflow or not.
     """
 
     C_TYPE = 'SimSensor'
-    C_NAME = 'Hopper9_Sensor'
+    C_NAME = 'Hopper10_Sensor'
   
     
 ## -------------------------------------------------------------------------------------------------      
     def _setup_function(self) -> TransferFunction:
-        _func = TF_BufferSensor(p_name='TF_Hopper9_Sensor',
+        _func = TF_BufferSensor(p_name='TF_Hopper10_Sensor',
                                 p_type=TransferFunction.C_TRF_FUNC_CUSTOM,
                                 p_dt=0,
-                                theta=0.8*9.1) # 80% of the maximum fill-level
+                                theta=0.8*10.0) # 80% of the maximum fill-level
         return _func
 
 
@@ -51,21 +51,21 @@ class Hopper9_Sensor(SimSensor):
                         
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Hopper9_FillLevel(SimState):
+class Hopper10_FillLevel(SimState):
     """
     This class serves as a component state to calculate the actual fill-level of the hopper.
     """
 
     C_TYPE = 'SimState'
-    C_NAME = 'Hopper9_FillLevel'
+    C_NAME = 'Hopper10_FillLevel'
   
     
 ## -------------------------------------------------------------------------------------------------      
     def _setup_function(self) -> TransferFunction:
-        _func = TF_FillLevel(p_name='TF_Hopper9_FillLevel',
+        _func = TF_FillLevel(p_name='TF_Hopper10_FillLevel',
                              p_type=TransferFunction.C_TRF_FUNC_CUSTOM,
                              p_dt=0,
-                             max_vol=9.1,
+                             max_vol=10.0,
                              min_vol=0)
         return _func
 
@@ -74,21 +74,21 @@ class Hopper9_FillLevel(SimState):
                         
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Hopper9_Overflow(SimState):
+class Hopper10_Overflow(SimState):
     """
     This class serves as a component state to calculate the overflow level of the hopper.
     """
 
     C_TYPE = 'SimState'
-    C_NAME = 'Hopper9_Overflow'
+    C_NAME = 'Hopper10_Overflow'
   
     
 ## -------------------------------------------------------------------------------------------------      
     def _setup_function(self) -> TransferFunction:
-        _func = TF_Overflow(p_name='TF_Hopper9_Overflow',
+        _func = TF_Overflow(p_name='TF_Hopper10_Overflow',
                             p_type=TransferFunction.C_TRF_FUNC_CUSTOM,
                             p_dt=0,
-                            max_vol=9.1)
+                            max_vol=10.0)
         return _func
 
 
@@ -96,7 +96,7 @@ class Hopper9_Overflow(SimState):
                         
 ## -------------------------------------------------------------------------------------------------
 ## -------------------------------------------------------------------------------------------------
-class Hopper9(Component):
+class Hopper10(Component):
 
 
 ## -------------------------------------------------------------------------------------------------
@@ -104,14 +104,14 @@ class Hopper9(Component):
         """
         A silo consists of two sensors and two states components.
         """
-        hopper_sensor = Hopper9_Sensor(p_name_short='Hopper9_Sensor1',
+        hopper_sensor = Hopper10_Sensor(p_name_short='Hopper10_Sensor1',
                                      p_base_set=Dimension.C_BASE_SET_Z,
                                      p_boundaries=[0,1])
-        hopper_fill_level = Hopper9_FillLevel(p_name_short='Hopper9_FillLevel',
+        hopper_fill_level = Hopper10_FillLevel(p_name_short='Hopper10_FillLevel',
                                             p_base_set=Dimension.C_BASE_SET_R,
                                             p_unit='L',
-                                            p_boundaries=[0,9.1])
-        hopper_overflow = Hopper9_Overflow(p_name_short='Hopper9_Overflow',
+                                            p_boundaries=[0,10.0])
+        hopper_overflow = Hopper10_Overflow(p_name_short='Hopper10_Overflow',
                                          p_base_set=Dimension.C_BASE_SET_R,
                                          p_unit='L',
                                          p_boundaries=[0,sys.maxsize])
