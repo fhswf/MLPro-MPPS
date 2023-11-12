565d5e00e07bc82403c585fe418e9094976128b7
steveyuwono <steveyuwono@gmail.com>
2023-11-12 22:59:37 +0100

    PS003_C026_Hopper_10L_SP.py #47

--- src/mlpro_mpps/pool/ml/gt_gameboard/GT001_LS_BGLP.py
+++ src/mlpro_mpps/pool/ml/gt_gameboard/GT001_LS_BGLP.py
@@ -18,4 +18,5 @@ This module provides a default implementation of the BGLP in MLPro-MPPS as RL En
 
 from mlpro_mpps.pool.mpps.PS003_LS_BGLP import LS_BGLP
 from mlpro.bf.math import *
-from mlpro.gt.models import *
+# from mlpro.gt.models import *
+from mlpro.gt.dynamicgames.basics import *
