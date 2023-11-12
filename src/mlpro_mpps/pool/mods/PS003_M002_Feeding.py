0e9e121fc8d709e6f4fe7d170754b10b12a027a7
steveyuwono <steveyuwono@gmail.com>
2023-11-12 15:18:37 +0100

    PS003_M002_Feeding.py #47

--- src/mlpro_mpps/pool/mods/PS003_M002_Feeding.py
+++ src/mlpro_mpps/pool/mods/PS003_M002_Feeding.py
@@ -1,7 +1,7 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - A Synoptic Framework for Standardized Machine Learning Tasks
 ## -- Package : mlpro_mpps.pool.mods
-## -- Module  : PS003_M001_Loading.py
+## -- Module  : PS003_M002_Feeding.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
@@ -12,14 +12,15 @@
 """
 Ver. 1.0.0 (2023-11-12)
 
-This module provides a default implementation of a module of the LS-BGLP, which is a Loading station.
+This module provides a default implementation of a module of the LS-BGLP, which is a Feeding station.
 """
 
 
 from mlpro_mpps.mpps import *
-from mlpro_mpps.pool.comps.PS003_C022_SiloLoading import *
-from mlpro_mpps.pool.comps.PS003_C002_Hopper_9L import *
-from mlpro_mpps.pool.comps.PS003_C009_ConveyorBelt1 import *
+from mlpro_mpps.pool.comps.PS003_C003_Silo_15L import *
+from mlpro_mpps.pool.comps.PS003_C004_Hopper_10L import *
+from mlpro_mpps.pool.comps.PS003_C011_VacuumPump1 import *
+from mlpro_mpps.pool.comps.PS003_C014_ScrewConveyor1 import *
 
 
                      
@@ -34,11 +35,13 @@ class LoadingStation(Module):
         """
         Loading station consists of a silo, a hopper, and a conveyor belt.
         """
-        silo = SiloLoading(p_name='SiloA')
-        hopper = Hopper9(p_name='HopperA')
-        belt = ConveyorBelt1(p_name='ActA1')
+        silo = Silo15(p_name='SiloB')
+        hopper = Hopper10(p_name='HopperB')
+        vc = VacuumPump1(p_name='ActB1')
+        belt = ScrewConveyor1(p_name='ActB2')
         
         self._add_component(p_component=silo)
         self._add_component(p_component=hopper)
+        self._add_component(p_component=vc)
         self._add_component(p_component=belt)
     
